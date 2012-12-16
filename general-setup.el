e3fe105b2211ea6552b19e37d7e2ce07ed2e5851
Dror Atariah <dror.atariah@fu-berlin.de>
2012-12-16 14:39:19 +0100

    Added loading the IDO mode

--- general-setup.el
+++ general-setup.el
@@ -94,6 +94,14 @@
 (setq ido-enable-flex-matching t) ; fuzzy matching is a must have
 (setq ido-enable-last-directory-history nil) ; forget latest selected directory names
 
+(custom-set-faces
+ '(ido-subdir ((t (:foreground "#D991CE")))) ;; Face used by ido for highlighting subdirs in the alternatives.
+;; '(ido-first-match ((t (:foreground "#ccff66")))) ;; Face used by ido for highlighting first match.
+;; '(ido-only-match ((t (:foreground "#ffcc33")))) ;; Face used by ido for highlighting only match.
+;; '(ido-indicator ((t (:foreground "#ffffff")))) ;; Face used by ido for highlighting its indicators (don't actually use this)
+;; '(ido-incomplete-regexp ((t (:foreground "#ffffff")))) ;; Ido face for indicating incomplete regexps. (don't use this either)
+)
+
 
 ;; Enable server
 (server-start)
