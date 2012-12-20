5bd74b071797db845816aaebbf021e5ce9410e69
Dror Atariah <dror.atariah@fu-berlin.de>
2012-12-20 06:58:27 +0100

    Added flyspell auto enabling in the log editing mode of magit

--- git-setup.el
+++ git-setup.el
@@ -4,3 +4,18 @@
 (add-to-list 'load-path
 	     "~/Library/elisp/magit-1.2.0")
 (require 'magit)
+
+(add-hook
+ 'magit-mode-hook
+ (lambda ()
+   (setq yas-dont-activate t)))
+
+(add-hook
+ 'magit-log-edit-mode-hook
+ (lambda ()
+   (setq fill-column 72)
+   (turn-on-auto-fill)))
+
+;; Add flyspell mode to the log edit mode of magit
+(add-hook 'magit-log-edit-mode-hook
+          (lambda () (flyspell-mode 1)))
