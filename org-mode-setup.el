1573c5533b8996479d8ee75eeb8d0fe6c26646db
Dror Atariah <dror.atariah@fu-berlin.de>
2013-01-03 09:12:35 +0100

    Customized the refiling mechanism according to the suggestions of Brent Hansen's (http://doc.norang.ca/org-mode.html#Refiling)

--- org-mode-setup.el
+++ org-mode-setup.el
@@ -51,9 +51,31 @@
 
 ;; Set common TAGS
 ;; Check: http://sachachua.com/blog/2008/01/tagging-in-org-plus-bonus-code-for-timeclocks-and-tags/
-(setq org-tag-alist '(("interesting_questions" . ?i)
-		      ("mittagseminar" . ?m)
-		      ))
+(setq org-tag-alist '(
+                      ("interesting_questions" . ?i)
+                      ("mittagseminar" . ?m)
+                      ("private" . ?p)
+                      ("work" .?w)
+                      ))
+
+;; Targets include this file and any file contributing to the agenda - up to 9 levels deep
+(setq org-refile-targets (quote ((nil :maxlevel . 9)
+                                 (org-agenda-files :maxlevel . 9))))
+
+;; Use full outline paths for refile targets - we file directly with IDO
+(setq org-refile-use-outline-path t)
+
+;; Targets complete directly with IDO
+(setq org-outline-path-complete-in-steps nil)
+
+;; Allow refile to create parent tasks with confirmation
+(setq org-refile-allow-creating-parent-nodes (quote confirm))
+
+;; Use IDO for both buffer and file completion and ido-everywhere to t
+(setq org-completion-use-ido t)
+(setq ido-everywhere t)
+(setq ido-max-directory-size 100000)
+(ido-mode (quote both))
 
 ;; set latexmk the default LaTeX compiler for org-mode and in general
 (setq org-latex-to-pdf-process (list "latexmk -f -pdf %f"))
