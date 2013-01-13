2b95c2dd55b663ecfb4f68a3c0007386ba2be876
Dror Atariah <dror.atariah@fu-berlin.de>
2013-01-13 21:59:05 +0100

    Completion of last commit/push

--- local_settings/yas-setup.el
+++ local_settings/yas-setup.el
@@ -3,15 +3,23 @@
 ;; http://code.google.com/p/yasnippet/
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Init:
-(add-to-list 'load-path
-	     "~/Library/elisp/yasnippet")
+
+;; (add-to-list 'load-path
+;; 	     "~/Library/elisp/yasnippet")
+
 (require 'yasnippet)
 (yas/global-mode 1)
 
-(yas/initialize)
+;; (yas/initialize)
 
 (setq yas/triggers-in-field t); Enable nested triggering of snippets
+
 ;; Develop and keep personal snippets
-(setq yas/root-directory "~/.emacs.d/packages/yas-snippets")
+(setq yas/root-directory "~/.emacs.d/my_packages/yas-snippets")
+
 ;; Load the snippets
 (yas/load-directory yas/root-directory)
+
+
+;; TODO: Fix the custom snippet as per:
+;; https://github.com/capitaomorte/yasnippet
