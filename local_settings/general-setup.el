b9df604e70e4d9e9882353834cd9947eba8e2c60
Dror Atariah <dror.atariah@fu-berlin.de>
2013-01-23 16:23:05 +0100

    Customized the frame's title to show full path and file name of current buffer.

--- local_settings/general-setup.el
+++ local_settings/general-setup.el
@@ -71,3 +71,8 @@
 
 ;; Enable server
 (server-start)
+
+;; Customize the frame's title
+(setq frame-title-format
+      (list (format "%s %%S: %%j " (system-name))
+            '(buffer-file-name "%f" (dired-directory dired-directory "%b"))))
