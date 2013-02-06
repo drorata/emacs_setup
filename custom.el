693e5155ab0660dc8d8c0d885c1666b9d4b4827a
Dror Atariah <dror.atariah@fu-berlin.de>
2013-02-06 22:09:20 +0100

    Customization made by org-drill

--- custom.el
+++ custom.el
@@ -3,4 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-drill-optimal-factor-matrix (quote ((1 (2.6 . 4.14))))))
+ '(org-drill-optimal-factor-matrix (quote ((1 (2.36 . 3.86) (2.5 . 4.0) (2.6 . 4.14))))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ido-first-match ((t (:foreground "red"))))
+ '(ido-only-match ((t (:foreground "green"))))
+ '(ido-subdir ((t (:foreground "yellow")))))
