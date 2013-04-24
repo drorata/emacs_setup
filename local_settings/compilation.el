72f9bb1d6f770b2929e5ffa83b45a4fde62a644a
Dror Atariah <dror.atariah@fu-berlin.de>
2013-04-24 09:08:56 +0200

    Improved the behavior of the compilation buffer

--- local_settings/compilation.el
+++ local_settings/compilation.el
@@ -1,21 +1,41 @@
 ;;;;;;;;;;;;;;;;;;
 ;; Compilation
 ;;;;;;;;;;;;;;;;;;
-(setq compilation-window-height 8)
-(setq compilation-finish-function
-      (lambda (buf str)
-        (if (string-match "exited abnormally" str)
-            ;;there were errors
-            (message "compilation errors, press C-x ` to visit")
-          ;;no errors
-          (progn
-            (if ( < (frame-width) 163)
-                ;; make the compilation window go away in 1.5 seconds
-                ;; only if the frame width is smaller then 85 chars.
-                (run-at-time 1.5 nil 'delete-windows-on buf)
-              )
-            (message "NO COMPILATION ERRORS! Thank you dear compiler..."))
-          )))
+
+(defun bury-compile-buffer-if-successful (buffer string)
+  "Bury a compilation buffer if succeeded without warnings "
+  (if (and
+       (string-match "compilation" (buffer-name buffer))
+       (string-match "finished" string)
+       (not
+        (with-current-buffer buffer
+          (search-forward "warning" nil t)))
+       )
+      (run-with-timer 1 nil
+                      (lambda (buf)
+                        (switch-to-prev-buffer (get-buffer-window buf) 'kill)
+                        (message "NO COMPILATION ERRORS! Thank you dear compiler...")
+                        ) buffer)
+    (message "compilation errors, press C-x ` to visit")
+    ))
+(add-hook 'compilation-finish-functions 'bury-compile-buffer-if-successful)
+
+;; (setq compilation-window-height 8)
+;; (setq compilation-finish-function
+;;       (lambda (buf str)
+;;         (if (string-match "exited abnormally" str)
+;;             ;;there were errors
+;;             (message "compilation errors, press C-x ` to visit")
+;;           ;;no errors
+;;           (progn
+;;             (if ( < (frame-width) 163)
+;;                 ;; make the compilation window go away in 1.5 seconds
+;;                 ;; only if the frame width is smaller then 85 chars.
+;;                 (run-at-time 1.5 nil 'delete-windows-on buf)
+;;               )
+;;             (message "NO COMPILATION ERRORS! Thank you dear compiler..."))
+;;           ))
+;;       )
 
 ;; Wraps lines in the compilation buffer.
 ;; http://stackoverflow.com/questions/1292936/line-wrapping-within-emacs-compilation-buffer
