eefc08dc08f6e77351164fe09bb37994fb12cf9f
Dror Atariah <dror.atariah@fu-berlin.de>
2013-05-03 16:13:55 +0200

    Reuse buffers with diredp mode

--- local_settings/dired_setup.el
+++ local_settings/dired_setup.el
@@ -3,3 +3,7 @@
 
 ;; Load dired+
 (require 'dired+)
+
+;; Reuse buffer with diredp (dired-plus)
+(toggle-diredp-find-file-reuse-dir 1)
+(define-key dired-mode-map  (kbd "<down-mouse-1>")    'diredp-mouse-find-file-reuse-dir-buffer)
