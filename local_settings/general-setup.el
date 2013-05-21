d78613ea5cc28eb4cb882f0b73e25c5774467f7f
Dror Atariah <dror.atariah@fu-berlin.de>
2013-05-21 14:47:26 +0200

    Enabled auto-backup http://www.emacswiki.org/emacs/BackupDirectory

--- local_settings/general-setup.el
+++ local_settings/general-setup.el
@@ -111,3 +111,12 @@
       (setq browse-url-generic-program "open")
       )
   )
+
+(setq
+ backup-by-copying t      ; don't clobber symlinks
+ backup-directory-alist
+ '(("." . "~/.saves-emacs"))    ; don't litter my fs tree
+ delete-old-versions t
+ kept-new-versions 6
+ kept-old-versions 2
+ version-control t)       ; use versioned backups
