d4a73cb6c7d21745212d626489474adc67a477df
Dror Atariah <dror.atariah@fu-berlin.de>
2013-06-12 13:07:56 +0200

    Added org-mode's repository

--- local_settings/general-setup.el
+++ local_settings/general-setup.el
@@ -34,6 +34,7 @@
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.milkbox.net/packages/")))
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 
 ;;---------------------------------------------------
 ;; Matching braces highlighting
