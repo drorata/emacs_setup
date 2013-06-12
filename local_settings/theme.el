cbf2507911426c44e34a7e54c88026a32358e634
Dror Atariah <dror.atariah@fu-berlin.de>
2013-06-12 13:07:41 +0200

    Cleaned the "theme" section. Made it simple and defined only colors

--- local_settings/theme.el
+++ local_settings/theme.el
@@ -1,32 +1,13 @@
+;; Set the background to gray
+(setq default-frame-alist
+      (append default-frame-alist
+              '((foreground-color . "Black")
+                (background-color . "#DEDEDE")
+                (cursor-color . "Black")
+                )))
 
-(set-background-color "#DEDEDE")
-
-;;; Theme customization
-;; (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/zenburn")
-;; (load-theme 'zenburn t)
-
-;; (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/tangotango")
-;; (load-theme 'tangotango t)
-;; ;; Fix the font that are changed by the theme
-;; (when (eq system-type 'darwin)
-;;   ;; default Latin font (e.g. Consolas)
-;;   ;; default font size (point * 10)
-;;   (set-face-attribute 'default nil :family "Monaco" :height 120 :weight 'normal))
-
-
-;; (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/ir-black")
-;; (load-theme 'ir-black t)
-
-;; (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/solarized")
-;; (load-theme 'solarized-light t)
-
-;; (load-theme 'tango-dark)
-
-;; ;; Setting Colors for various additional elements
-;; (defface hl-line '((t (:background "white")))
-;;   "Face to use for `hl-line-face'." :group 'hl-line)
-;; (setq hl-line-face 'hl-line)
-;; (global-hl-line-mode t)
-;; ;;(set-face-background 'hl-line-face "gray")
-;; (set-face-background 'show-paren-match-face "yellow")
-;; (set-face-background 'region "#e84687")
+;; Custom the faces of diffs in vc-diff mode
+(custom-set-faces
+ '(diff-added ((t (:foreground "dark green"))) 'now)
+ '(diff-removed ((t (:foreground "red"))) 'now)
+ )
