4a11d6694ad4408c806509edb9e7e93cc8affb66
Dror Atariah <dror.atariah@fu-berlin.de>
2013-08-16 10:57:32 +0200

    Trying out better scrolling settings

--- local_settings/improved-scrolling.el
+++ local_settings/improved-scrolling.el
@@ -3,12 +3,23 @@
 ;; Scroll just one line when hitting bottom of window
 (setq scroll-conservatively 10000)
 
+(setq scroll-margin 1
+      scroll-conservatively 0
+      scroll-up-aggressively 0.01
+      scroll-down-aggressively 0.01)
+(setq-default scroll-up-aggressively 0.01
+              scroll-down-aggressively 0.01)
+
 (setq scroll-step 1)
 (setq scroll-conservatively 10000)
 (setq auto-window-vscroll nil)
 
-(setq redisplay-dont-pause t
-      scroll-margin 1
-      scroll-step 1
-      scroll-conservatively 10000
-      scroll-preserve-screen-position 1)
+;; (setq scroll-step 1)
+;; (setq scroll-conservatively 10000)
+;; (setq auto-window-vscroll nil)
+
+;; (setq redisplay-dont-pause t
+;;       scroll-margin 1
+;;       scroll-step 1
+;;       scroll-conservatively 10000
+;;       scroll-preserve-screen-position 1)
