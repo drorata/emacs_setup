372b945869f067b6f8dacae9028ff87f9964e8b7
Dror Atariah <dror.atariah@fu-berlin.de>
2013-09-05 13:55:27 +0200

    Verbose lines in dired

--- local_settings/dired_setup.el
+++ local_settings/dired_setup.el
@@ -7,3 +7,14 @@
 ;; Reuse buffer with diredp (dired-plus)
 (toggle-diredp-find-file-reuse-dir 1)
 (define-key dired-mode-map  (kbd "<down-mouse-1>")    'diredp-mouse-find-file-reuse-dir-buffer)
+
+;; Do not line wrap! Otherwise won't scroll when lines are too long
+(add-hook 'dired-mode-hook '(lambda () (toggle-truncate-lines 1)))
+
+;; Verbose columns in dired
+;; See: http://www.emacswiki.org/emacs/LsLispToggleVerbosity
+;; See: http://stackoverflow.com/questions/4115465/emacs-dired-too-much-information
+(require 'ls-lisp)
+(setq ls-lisp-use-insert-directory-program nil)
+(custom-set-variables
+ '(ls-lisp-verbosity nil))
