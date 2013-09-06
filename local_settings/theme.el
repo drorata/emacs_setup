b97c7ffc5c94f2c70e9d70c6b0c22fb96130497e
Dror Atariah <dror.atariah@fu-berlin.de>
2013-09-06 09:37:10 +0200

    Added ido-color settings to the theme.el

--- local_settings/theme.el
+++ local_settings/theme.el
@@ -6,8 +6,24 @@
                 (cursor-color . "Black")
                 )))
 
+;; (load-theme 'tsdh-dark)
+
 ;; Custom the faces of diffs in vc-diff mode
 (custom-set-faces
  '(diff-added ((t (:foreground "dark green"))) 'now)
  '(diff-removed ((t (:foreground "red"))) 'now)
+ )
+
+;; ido colors settings
+(custom-set-faces
+ ;; Face used by ido for highlighting subdirs in the alternatives.
+ '(ido-subdir ((t (:foreground "blue"))))
+ ;; Face used by ido for highlighting first match.
+ '(ido-first-match ((t (:foreground "red"))))
+ ;; Face used by ido for highlighting only match.
+ '(ido-only-match ((t (:foreground "darkgreen"))))
+ ;; Face used by ido for highlighting its indicators (don't actually use this)
+ ;; '(ido-indicator ((t (:foreground "#ffffff"))))
+ ;; Ido face for indicating incomplete regexps. (don't use this either)
+ ;; '(ido-incomplete-regexp ((t (:foreground "#ffffff"))))
  )
