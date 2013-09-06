5f4e15e8fcea3bebc745e11ccdc8a8d8cbefc1e3
Dror Atariah <dror.atariah@fu-berlin.de>
2013-09-06 09:35:33 +0200

    Moved ido-colors settings to the theme.el file

--- local_settings/ido-setup.el
+++ local_settings/ido-setup.el
@@ -19,16 +19,3 @@
  ido-use-url-at-point nil            ;; don't use url at point
  ido-enable-prefix nil
  ido-max-prospects 12)
-
-(custom-set-faces
- ;; Face used by ido for highlighting subdirs in the alternatives.
- '(ido-subdir ((t (:foreground "blue"))))
- ;; Face used by ido for highlighting first match.
- '(ido-first-match ((t (:foreground "red"))))
- ;; Face used by ido for highlighting only match.
- '(ido-only-match ((t (:foreground "darkgreen"))))
- ;; Face used by ido for highlighting its indicators (don't actually use this)
- ;; '(ido-indicator ((t (:foreground "#ffffff"))))
- ;; Ido face for indicating incomplete regexps. (don't use this either)
- ;; '(ido-incomplete-regexp ((t (:foreground "#ffffff"))))
-)
