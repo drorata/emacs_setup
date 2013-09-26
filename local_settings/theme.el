9a0d9052ef1fd147f790676008ff165464e04a70
Dror Atariah <dror.atariah@fu-berlin.de>
2013-09-26 08:33:02 +0200

    Collected all appearances related elements to one file

--- local_settings/theme.el
+++ local_settings/theme.el
@@ -13,6 +13,18 @@
         ))
 
 (custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(diff-added ((t (:foreground "dark green"))))
+ '(diff-removed ((t (:foreground "red"))))
+ '(ido-first-match ((t (:foreground "red"))))
+ '(ido-only-match ((t (:foreground "darkgreen"))))
+ '(ido-subdir ((t (:foreground "blue"))))
+ '(org-clock-overlay ((t (:background "gridColor" :foreground "black")))))
+
+(custom-set-faces
  ;; vc-colors
  '(diff-added ((t (:foreground "darkgreen"))) 'now)
  '(diff-removed ((t (:foreground "red"))) 'now)
@@ -27,6 +39,10 @@
  ;; '(ido-indicator ((t (:foreground "#ffffff"))))
  ;; Ido face for indicating incomplete regexps. (don't use this either)
  ;; '(ido-incomplete-regexp ((t (:foreground "#ffffff"))))
+ )
+
+(custom-set-faces
+ '(org-clock-overlay ((t (:background "gridColor" :foreground "black"))))
  )
 
 ;; change magit diff colors
