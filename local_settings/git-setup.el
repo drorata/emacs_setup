55274bbc7822009e038fff6c6cbffc91545aac8e
Dror Atariah <dror.atariah@fu-berlin.de>
2013-09-26 08:20:27 +0200

    Moved magit faces defs to dedicated file

--- local_settings/git-setup.el
+++ local_settings/git-setup.el
@@ -18,14 +18,6 @@
 (add-hook 'magit-log-edit-mode-hook
           (lambda () (flyspell-mode 1)))
 
-;; change magit diff colors
-(eval-after-load 'magit
-  '(progn
-     (set-face-foreground 'magit-diff-add "green")
-     (set-face-foreground 'magit-diff-del "red")
-     (when (not window-system)
-       (set-face-background 'magit-item-highlight "black"))))
-
 (global-set-key  (kbd "C-c C-v") 'magit-status)
 
 ;; TODO: Disable yasnippet in magit-mode
