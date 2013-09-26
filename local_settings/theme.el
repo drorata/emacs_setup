b03824876da70f7b992aec78018500cb2e40c55e
Dror Atariah <dror.atariah@fu-berlin.de>
2013-09-26 08:20:46 +0200

    Reverted to gray background

--- local_settings/theme.el
+++ local_settings/theme.el
@@ -1,29 +1,38 @@
-;; Set the background to gray
-;; (setq default-frame-alist
-;;       (append default-frame-alist
-;;               '((foreground-color . "Black")
-;;                 (background-color . "#DEDEDE")
-;;                 (cursor-color . "Black")
-;;                 )))
-
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-(load-theme `dark-emacs)
-
+;; (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+;; (load-theme `dark-emacs t)
 ;; (load-theme 'tsdh-dark)
 
-;; Custom the faces of diffs in vc-diff mode
+;; Set the background to gray
+(setq default-frame-alist
+(append default-frame-alist
+        '(
+          (foreground-color . "black")
+          (background-color . "#DEDEDE")
+          (cursor-color . "black")
+          )
+        ))
+
 (custom-set-faces
- '(diff-added ((t (:foreground "dark green"))) 'now)
+ ;; vc-colors
+ '(diff-added ((t (:foreground "darkgreen"))) 'now)
  '(diff-removed ((t (:foreground "red"))) 'now)
  ;; ido colors settings
  ;; Face used by ido for highlighting subdirs in the alternatives.
- '(ido-subdir ((t (:foreground "#0084F0"))))
+ '(ido-subdir ((t (:foreground "blue"))))
  ;; Face used by ido for highlighting first match.
  '(ido-first-match ((t (:foreground "red"))))
  ;; Face used by ido for highlighting only match.
- '(ido-only-match ((t (:foreground "green"))))
+ '(ido-only-match ((t (:foreground "darkgreen"))))
  ;; Face used by ido for highlighting its indicators (don't actually use this)
  ;; '(ido-indicator ((t (:foreground "#ffffff"))))
  ;; Ido face for indicating incomplete regexps. (don't use this either)
  ;; '(ido-incomplete-regexp ((t (:foreground "#ffffff"))))
  )
+
+;; change magit diff colors
+(eval-after-load 'magit
+  '(progn
+     (set-face-foreground 'magit-diff-add "darkgreen")
+     (set-face-foreground 'magit-diff-del "red")
+     (when (not window-system)
+       (set-face-background 'magit-item-highlight "black"))))
