8c2e1d17eb44693f2d66713bd50112227fd7d533
Dror Atariah <dror.atariah@fu-berlin.de>
2013-10-05 14:49:13 +0200

    Cleaned and re-organized the latex settings

--- local_settings/latex-related-setup.el
+++ local_settings/latex-related-setup.el
@@ -1,98 +1,54 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;LaTeX Related
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
+;;
+;; General Setting
+;;
 (setq TeX-PDF-mode t)
 (setq TeX-auto-save t)
 (setq TeX-parse-self t)
 (setq-default TeX-master nil) ; Master file support
 
+;;
+;; Hooks
+;;
 ;; Line breaking and visualizing
 (add-hook 'LaTeX-mode-hook 'visual-line-mode)
-
-;; Enable refTeX
-(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
-(setq reftex-plug-into-AUCTeX t)
-
 ;; Enable yas in LaTeX mode
 (add-hook 'LaTeX-mode-hook 'yas-minor-mode)
-
 ;; Enable LaTeX-math-mode by default
 (add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
-
 ;; Auto enable flyspell-mode
 (add-hook 'LaTeX-mode-hook 'turn-on-flyspell)
-(setq flyspell-auto-correct-binding (kbd "C-~")) ; or a binding of your choice
+;; Swap {/[ and }/]
+;; See: http://superuser.com/q/634564/57249 for further details.
+(require 'swap-braces)
+(add-hook 'LaTeX-mode-hook 'swap-braces-mode)
 
-;; Enable clevref in tex-mode
-(defun reftex-format-cref (label def-fmt)
-  (format "\\cref{%s}" label))
-(setq reftex-format-ref-function 'reftex-format-cref)
-
-;; So that RefTeX recognizes bibliographies in \addbibresource
-(setq reftex-bibliography-commands (quote ("addbibresource")))
-
-(setq reftex-label-alist
-      '(
-        ("claim" ?h "th:" nil t)
-        ("theorem" ?h "th:" nil t)
-        ("lemma" ?h "th:" nil t)
-        ("corollary" ?h "th:" nil t)
-        ("proposition" ?h "th:" nil t)
-        ("condition" ?h "cond:" nil t)
-        ("definition" ?d "def:" nil t)
-        ("remark" ?r "rmrk:" nil t)
-        )
-      )
-
+;;
 ;; Adds environments to the C-c C-e
+;;
 ;; http://superuser.com/questions/122410/adding-autocomplete-options-to-auctex-c-c-c-e
 ;; See: http://www.gnu.org/software/auctex/manual/auctex/Adding-Environments.html#Adding-Environments
 ;; for further details
 (add-hook 'LaTeX-mode-hook
           (lambda ()
             (LaTeX-add-environments
-	     '("tikzpicture")
-	     '("scope")
-	     '("align" LaTeX-env-label)
-	     '("align*")
-	     '("aligned" LaTeX-env-label)
-	     '("theorem" LaTeX-env-label)
-	     '("lemma" LaTeX-env-label)
-	     '("corollary" LaTeX-env-label)
-	     '("proof")
-	     '("remark" LaTeX-env-label)
-	     '("claim" LaTeX-env-label)
-	     '("definition" LaTeX-env-label)
-	     )
-	    )
-	  )
-
-;; Enable outline-mode in LaTeX-mode
-(defun turn-on-outline-minor-mode ()
-  (outline-minor-mode 1))
-(add-hook 'LaTeX-mode-hook 'turn-on-outline-minor-mode)
-(add-hook 'latex-mode-hook 'turn-on-outline-minor-mode)
-(setq outline-minor-mode-prefix "\C-c\C-o") ; Or something else
-
-;; Add support of latexmk
-;; First add the texbin to the path
-(setenv "PATH"
-        (concat
-         "/usr/texbin" ":"
-         (getenv "PATH")))
-;; Config the TeX-command-list
-(add-hook 'LaTeX-mode-hook (lambda ()
-                             (push
-                              '("Latexmk" "latexmk -pdf %s" TeX-run-TeX nil t
-                                :help "Run Latexmk on file")
-                              TeX-command-list)))
-
-'("%(-PDF)"
-  (lambda ()
-    (if (and (not TeX-Omega-mode)
-             (or TeX-PDF-mode TeX-DVI-via-PDFTeX))
-        "-pdf" "")))
+             '("tikzpicture")
+             '("scope")
+             '("align" LaTeX-env-label)
+             '("align*")
+             '("aligned" LaTeX-env-label)
+             '("theorem" LaTeX-env-label)
+             '("lemma" LaTeX-env-label)
+             '("corollary" LaTeX-env-label)
+             '("proof")
+             '("remark" LaTeX-env-label)
+             '("claim" LaTeX-env-label)
+             '("definition" LaTeX-env-label)
+             )
+            )
+          )
 
 ;; This part should bind C-0 to the following seuqence:
 ;; 1. Save the master file and all depending ones
@@ -114,18 +70,16 @@
     (if (plist-get TeX-error-report-switches (intern master-file))
         (TeX-next-error t)
       (minibuffer-message "latexmk done"))))
+(add-hook 'LaTeX-mode-hook
+          (lambda () (local-set-key (kbd "C-0") #'run-latexmk)))
 
-;; Key bindings for LaTeX mode
-(add-hook 'LaTeX-mode-hook
-          (lambda () (local-set-key (kbd "C-0") #'run-latexmk))
-          )
-(add-hook 'LaTeX-mode-hook
-          (lambda () (local-set-key (kbd "C-c C-v") 'magit-status))
-          )
-(add-hook 'LaTeX-mode-hook
-          (lambda () (local-set-key (kbd "C-c v") 'TeX-view))
-          )
 
+;; Enable outline-mode in LaTeX-mode
+(defun turn-on-outline-minor-mode ()
+  (outline-minor-mode 1))
+(add-hook 'LaTeX-mode-hook 'turn-on-outline-minor-mode)
+(add-hook 'latex-mode-hook 'turn-on-outline-minor-mode)
+(setq outline-minor-mode-prefix "\C-c\C-o") ; Or something else
 ;; Automatically activate TeX-fold-mode.
 ;; http://tex.stackexchange.com/questions/52179/what-is-your-favorite-emacs-and-or-auctex-command-trick
 (add-hook 'TeX-mode-hook
@@ -133,6 +87,7 @@
 
 ;;
 ;; Set additional fontifications of macros
+;;
 ;; http://tex.stackexchange.com/questions/50827/a-simpletons-guide-to-tex-workflow-with-emacs
 (setq font-latex-match-reference-keywords
       '(("cref" "*[{")
@@ -141,8 +96,9 @@
         ("includegraphics" "[{")
         ("citeauthor" "[{")))
 
-;;;;;
+;;
 ;; Enable emacs->skim sync
+;;
 ;; For this to work, TeX-PDF-mode should be enabled!
 (setq TeX-view-program-selection '((output-pdf "PDF Viewer")))
 (setq TeX-view-program-list
@@ -153,18 +109,64 @@
           )
 
 ;; Key bindings in TeX-mode
-;; (add-hook 'TeX-mode-hook (lambda () (define-key TeX-mode-map "<M-tab>" 'TeX-complete-macro)))
+(add-hook 'LaTeX-mode-hook
+          (lambda () (local-set-key (kbd "C-c C-v") 'magit-status)))
+(add-hook 'LaTeX-mode-hook
+          (lambda () (local-set-key (kbd "C-c v") 'TeX-view)))
 (add-hook 'LaTeX-mode-hook
           (lambda ()
-            (local-set-key (kbd "M-RET") 'LaTeX-insert-item)
-            )
-          )
-
-;; Swap {/[ and }/]
-;; See: http://superuser.com/q/634564/57249 for further details.
-(require 'swap-braces)
-(add-hook 'LaTeX-mode-hook 'swap-braces-mode)
+            (local-set-key (kbd "M-RET") 'LaTeX-insert-item)))
 
 ;; " expands into csquotes macros
 (setq LaTeX-csquotes-close-quote "}"
       LaTeX-csquotes-open-quote "\\enquote{")
+
+;;
+;; RefTeX related
+;;
+;; Enable refTeX
+(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+(setq reftex-plug-into-AUCTeX t)
+
+;; Disable initial prompt for style
+(setq reftex-ref-macro-prompt nil)
+
+;; Enable clevref in tex-mode
+(defun reftex-format-cref (label def-fmt)
+  (format "\\cref{%s}" label))
+(setq reftex-format-ref-function 'reftex-format-cref)
+(setq reftex-format-ref-function nil)
+
+(eval-after-load
+    "latex"
+  '(TeX-add-style-hook
+    "cleveref"
+    (lambda ()
+      (if (boundp 'reftex-ref-style-alist)
+          (add-to-list
+           'reftex-ref-style-alist
+           '("Cleveref" "cleveref"
+             (("\\cref" ?c) ("\\Cref" ?C) ("\\cpageref" ?d) ("\\Cpageref" ?D)))))
+      (add-to-list 'reftex-ref-style-default-list "Cleveref")
+      (TeX-add-symbols
+       '("cref" TeX-arg-ref)
+       '("Cref" TeX-arg-ref)
+       '("cpageref" TeX-arg-ref)
+       '("Cpageref" TeX-arg-ref)))))
+
+;; So that RefTeX recognizes bibliographies in \addbibresource
+(setq reftex-bibliography-commands (quote ("addbibresource")))
+
+(setq reftex-label-alist
+      '(
+        ("claim" ?h "th:" nil t)
+        ("theorem" ?h "th:" nil t)
+        ("lemma" ?h "th:" nil t)
+        ("corollary" ?h "th:" nil t)
+        ("proposition" ?h "th:" nil t)
+        ("condition" ?h "cond:" nil t)
+        ("definition" ?d "def:" nil t)
+        ("remark" ?r "rmrk:" nil t)
+        )
+      )
+;; RefTex - end
