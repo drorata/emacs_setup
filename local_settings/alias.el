648c1cb4d4caf838b70d1c5865e54e62b2bb5991
Dror Atariah <dror.atariah@fu-berlin.de>
2013-12-12 09:39:30 +0100

    Added list of aliases
    Inspired by http://ergoemacs.org/emacs/emacs_alias.html

---
+++ local_settings/alias.el
@@ -0,0 +1,19 @@
+;; Idea taken from http://ergoemacs.org/emacs/emacs_alias.html
+
+(defalias 'mg 'magit-status)
+(defalias 'gitst 'magit-status)
+(defalias 'git 'magit-status)
+(defalias 'ir 'indent-region)
+
+; elisp
+(defalias 'eb 'eval-buffer)
+(defalias 'er 'eval-region)
+(defalias 'ed 'eval-defun)
+(defalias 'eis 'elisp-index-search)
+(defalias 'lf 'load-file)
+
+; minor modes
+(defalias 'wsm 'whitespace-mode)
+(defalias 'gwsm 'global-whitespace-mode)
+(defalias 'vlm 'visual-line-mode)
+(defalias 'glm 'global-linum-mode)
