916a0754a7099584d6eb9b6603f0268ed92deb5e
Dror Atariah <dror.atariah@fu-berlin.de>
2014-12-03 21:55:25 +0100

    Improved path management to find pdflatex under OSX 10.9+

--- local_settings/general-setup.el
+++ local_settings/general-setup.el
@@ -141,3 +141,19 @@
 ;; See appearance.el for face customization
 (require 'anzu)
 (global-anzu-mode t)
+
+;;
+;; Set the emacs path to the bash one. This solves the problem
+;; that org cannot export to PDF on OS X 10.9+.
+;; See: http://emacs.stackexchange.com/a/4093/861
+(defun set-exec-path-from-shell-PATH ()
+  "Sets the exec-path to the same value used by the user shell"
+  (let ((path-from-shell
+         (replace-regexp-in-string
+          "[[:space:]\n]*$" ""
+          (shell-command-to-string "$SHELL -l -c 'echo $PATH'"))))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+
+;; call function now
+(set-exec-path-from-shell-PATH)
