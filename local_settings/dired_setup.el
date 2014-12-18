c697904d3ba3995ebfee4766d450447655a00468
Dror Atariah <dror.atariah@fu-berlin.de>
2014-12-18 22:14:44 +0100

    Cleaned code and fixed verbose listing

--- local_settings/dired_setup.el
+++ local_settings/dired_setup.el
@@ -6,15 +6,12 @@
 
 ;; Reuse buffer with diredp (dired-plus)
 (toggle-diredp-find-file-reuse-dir 1)
-(define-key dired-mode-map  (kbd "<down-mouse-1>")    'diredp-mouse-find-file-reuse-dir-buffer)
+(define-key dired-mode-map  (kbd "<down-mouse-1>")
+  'diredp-mouse-find-file-reuse-dir-buffer)
 
 ;; Do not line wrap! Otherwise won't scroll when lines are too long
-(add-hook 'dired-mode-hook '(lambda () (toggle-truncate-lines 1)))
+(add-hook 'dired-mode-hook
+          '(lambda () (toggle-truncate-lines 1)))
 
-;; Verbose columns in dired
-;; See: http://www.emacswiki.org/emacs/LsLispToggleVerbosity
-;; See: http://stackoverflow.com/questions/4115465/emacs-dired-too-much-information
-(require 'ls-lisp)
-(setq ls-lisp-use-insert-directory-program nil)
-(custom-set-variables
- '(ls-lisp-verbosity nil))
+(add-hook 'dired-mode-hook
+          '(lambda () (dired-hide-details-mode 0)))
