5c603434c19a4cdebe56317777f637bbc72e05c3
Dror Atariah <dror.atariah@fu-berlin.de>
2014-12-20 00:25:53 +0100

    Masked aliases with a da prefix

--- local_settings/alias.el
+++ local_settings/alias.el
@@ -1,23 +1,22 @@
 ;; Idea taken from http://ergoemacs.org/emacs/emacs_alias.html
 
-(defalias 'mg 'magit-status)
-(defalias 'gitst 'magit-status)
-(defalias 'git 'magit-status)
-(defalias 'ir 'indent-region)
-(defalias 'pull 'org-mobile-pull)
-;; 2FIX
-;; (defalias 'push 'org-mobile-push)
-(defalias 'oa 'org-archive-subtree)
+(defalias 'da/mg 'magit-status)
+(defalias 'da/gitst 'magit-status)
+(defalias 'da/git 'magit-status)
+(defalias 'da/ir 'indent-region)
+(defalias 'da/pull 'org-mobile-pull)
+(defalias 'da/push 'org-mobile-push)
+(defalias 'da/oa 'org-archive-subtree)
 
 ; elisp
-(defalias 'eb 'eval-buffer)
-(defalias 'er 'eval-region)
-(defalias 'ed 'eval-defun)
-(defalias 'eis 'elisp-index-search)
-(defalias 'lf 'load-file)
+(defalias 'da/eb 'eval-buffer)
+(defalias 'da/er 'eval-region)
+(defalias 'da/ed 'eval-defun)
+(defalias 'da/eis 'elisp-index-search)
+(defalias 'da/lf 'load-file)
 
 ; minor modes
-(defalias 'wsm 'whitespace-mode)
-(defalias 'gwsm 'global-whitespace-mode)
-(defalias 'vlm 'visual-line-mode)
-(defalias 'glm 'global-linum-mode)
+(defalias 'da/wsm 'whitespace-mode)
+(defalias 'da/gwsm 'global-whitespace-mode)
+(defalias 'da/vlm 'visual-line-mode)
+(defalias 'da/glm 'global-linum-mode)
