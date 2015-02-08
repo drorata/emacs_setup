cbd2ffa945c960ea1d0a2876ed240e316c0a63b1
Dror Atariah <dror.atariah@fu-berlin.de>
2015-02-08 12:55:55 +0100

    Moved .emacs to .emacs.d/init.el

---
+++ init.el
@@ -0,0 +1,8 @@
+;; (add-to-list 'load-path "~/.emacs.d")
+
+;; (setq debug-on-error t)
+(load "~/.emacs.d/main_setup")
+
+(setq ns-pop-up-frames nil)
+(put 'downcase-region 'disabled nil)
+(put 'narrow-to-region 'disabled nil)
