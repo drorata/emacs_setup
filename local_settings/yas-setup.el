f79217f6592268def986c9c3690b54eb7aeb3894
Dror Atariah <dror.atariah@fu-berlin.de>
2015-04-27 22:03:28 +0200

    Setup YAS snippets in a canonical manner

--- local_settings/yas-setup.el
+++ local_settings/yas-setup.el
@@ -4,22 +4,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Init:
 
-;; (add-to-list 'load-path
-;; 	     "~/Library/elisp/yasnippet")
-
 (require 'yasnippet)
 (yas/global-mode 1)
 
-;; (yas/initialize)
-
 (setq yas/triggers-in-field t); Enable nested triggering of snippets
-
-;; Develop and keep personal snippets
-(setq yas/root-directory "~/.emacs.d/my_packages/yas-snippets")
-
-;; Load the snippets
-(yas/load-directory yas/root-directory)
-
-
-;; TODO: Fix the custom snippet as per:
-;; https://github.com/capitaomorte/yasnippet
